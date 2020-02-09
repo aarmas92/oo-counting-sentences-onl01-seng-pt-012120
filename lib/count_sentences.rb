68b28e46c09b8f54d617cd162bd4f90c2aab55fa
Alvaro Armas <aarmas92@outlook.com>
2020-02-09 21:19:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,26 +7,28 @@ class String
       return true
     else
       return false
+    end
   end
-  
+
   def question?
-    if self.ends_with?("?")
+    if self.end_with?("?")
       return true
     else
       return false
-    end  
+    end
   end
 
   def exclamation?
-    if self.ends_with?("!")
+    if self.end_with?("!")
       return true
     else
       return false
-    end  
+    end
   end
 
   def count_sentences
     new_array = self.split(/[.?!]/).reject {|string| string.empty?}
     return new_array.length
   end
+end
 end
